f467e9b96cbf12eedb946a7d5003ec5c4cecfb90
Yuri.Batanov <yurok_37@icloud.com>
2025-04-12 23:10:55 +0500

    update function for admin check

--- bot_app/utils/admin_check.py
+++ bot_app/utils/admin_check.py
@@ -1,23 +1,20 @@
-from aiogram import Dispatcher
+from aiogram import Bot
 from aiogram.exceptions import TelegramBadRequest
 
 from config.log import logger
 
 
-async def check_is_admin(dp: Dispatcher,
+async def check_is_admin(bot: Bot,
                          user_id: int,
                          groups_id: list[int]) -> bool:
 
     """
     Проверка пользователя на то, что он является администратором в группах.
-    :param dp: Объект Dispatcher.
+    :param bot: Объект Bot.
     :param user_id: ID проверяемого пользователя.
     :param groups_id: ID групп, в которых проверяется пользователь.
     :return:
     """
-
-    # Создаём объект бота из Dispatcher
-    bot = dp['bot']
 
     # Осуществляем проверку по каждой группе из БД
     for group_id in groups_id:
@@ -28,8 +25,7 @@ async def check_is_admin(dp: Dispatcher,
                 user_id
             )
             # Если пользователь администратор
-            is_admin = await member.is_chat_admin()
-            if is_admin:
+            if member.status in ['administrator', 'admin', 'creator']:
                 return True
         except TelegramBadRequest:
             logger.error('Ошибка проверки пользователя на администратора в группах.')
