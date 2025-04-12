94184d964ee53bb113745c3f1c96ff27ca6b0838
Yuri.Batanov <yurok_37@icloud.com>
2025-04-12 23:17:44 +0500

    update log.py

--- config/log.py
+++ config/log.py
@@ -3,15 +3,23 @@ import logging
 from logging.handlers import RotatingFileHandler
 
 
+# Определяем корневую директорию проекта
+BASE_DIR = os.path.abspath(os.path.join(os.path.dirname(__file__), ".."))
+
 # Путь к директории логов
-log_dir = os.path.join(os.path.dirname(__file__), '..', '..', 'logs')
-log_file = os.path.join(log_dir, 'bot_log')
+log_dir = os.path.join(BASE_DIR, 'logs')
+log_file = os.path.join(log_dir, 'bot.log')
 
 # Создание папки logs
 os.makedirs(log_dir, exist_ok=True)
 
 # Обработчик для ротации логов (5 файлов по 1 MB каждый)
-file_handler = RotatingFileHandler(log_file, maxBytes=1024 * 1024, backupCount=5)
+file_handler = RotatingFileHandler(
+    log_file,
+    maxBytes=1024 * 1024,
+    backupCount=5,
+    encoding="utf-8"
+)
 
 # Формат логов
 formatter = logging.Formatter(
@@ -21,8 +29,10 @@ file_handler.setFormatter(formatter)
 
 # Настройка логирования
 logging.basicConfig(
-    level=logging.INFO,  # Уровень логов: DEBUG, INFO, WARNING, ERROR, CRITICAL
+    # Уровень логов: DEBUG, INFO, WARNING, ERROR, CRITICAL
+    level=logging.INFO,
     handlers=[file_handler],
+    force=True
 )
 
 # Логгер для использования в проекте
